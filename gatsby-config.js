eeb61f1f11d099f427451f884e1ed97cea3cccd6
Coldboltage <alanreid@hotmail.co.uk>
2021-04-10 15:35:44 +0100

    Testing config settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
         // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
-          `https://fvs.5ec.myftpupload.com/graphql/`,
+          `http://fvs.5ec.myftpupload.com/graphql/`,
       },
     },
     {
