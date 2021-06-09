aa0d17580071989dd709f1e96f9f9851f1c9c228
Coldboltage <alanreid@hotmail.co.uk>
2021-06-09 10:09:23 +0100

    Fixed firstCard styles and read more

--- src/components/FirstPostCard.js
+++ src/components/FirstPostCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "gatsby"
-import {  GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage } from "gatsby-plugin-image"
 import * as styles from "../styles/PostCard.module.css"
 
 const FirstPostCard = ({ image, data }) => {
@@ -8,19 +8,22 @@ const FirstPostCard = ({ image, data }) => {
     const name = categories.nodes[0].name
     console.log(categories.nodes[0].name)
     return (
-        <div>
-            <Link to={`/${slug}`}>
-                <GatsbyImage className={styles.firstImage} image={image} alt="first post" />
-            </Link>
-            <div className={styles.innerContainer}>
-                <div className={styles.taxomonies}>
-                    <span><Link className={styles.link} to={`/${slug}`}>{name}</Link></span><span> - </span><Link className={styles.date} to={`/${slug}`}>{date}</Link>
+        <Link to={`/${slug}`} className={styles.firstCard}>
+            <div class={styles.firstCardStyles}>
+                <Link to={`/${slug}`}>
+                    <GatsbyImage className={styles.firstImage} image={image} alt="first post" />
+                </Link>
+                <div className={styles.innerContainer}>
+                    <div className={styles.taxomonies}>
+                        <span><Link className={styles.link} to={`/${slug}`}>{name}</Link></span><span> - </span><Link className={styles.date} to={`/${slug}`}>{date}</Link>
+                    </div>
+                    <h3>{title}</h3>
+                    <div dangerouslySetInnerHTML={{ __html: excerpt }}></div>
+                    {/* <Link to={`/${slug}`}>Read more</Link> */}
                 </div>
-                <h3>{title}</h3>
-                <div dangerouslySetInnerHTML={{ __html: excerpt }}></div>
-                <Link to={`/${slug}`}>Read more</Link>
             </div>
-        </div>
+        </Link>
+
     )
 }
 
